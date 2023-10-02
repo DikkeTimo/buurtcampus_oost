cdbe0a265540672db59c38bd3a0ea88167772adc
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-10-02 11:05:54 +0200

    changes routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,8 @@
 <script>
-	import { onMount } from 'svelte';
-	import { request, gql } from 'graphql-request';
-
-	const fetchData = async () => {
-		const document = gql`
-			query MyQuery {
-				stekjes {
-					aanmelddatum
-					beschrijving
-					landvanherkomst
-					naam
-					temperatuur
-					zonlicht
-					watergeven
-					voeding
-					verpotten
-				}
-			}
-		`;
-
-		const data = await request(
-			'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbe0wlb32hx401ui0c2yfm49/master',
-			document
-		);
-	};
-
-	onMount(fetchData);
+  import Workshops from '../components/workshops.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Workshops />
+
+
+
