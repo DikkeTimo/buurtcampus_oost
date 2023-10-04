14539a1f4af1c350dd393ad9ca6340ea67f7a9fe
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-10-04 19:29:56 +0200

    Update +page.svelte

--- src/routes/all_stekjes/+page.svelte
+++ src/routes/all_stekjes/+page.svelte
@@ -2,3 +2,17 @@
 	export let data;
 	console.log(data)
 </script>
+
+<section class="intro_text">
+	<!-- introduction text -->
+
+</section>
+
+<section class="wrapper">
+	<!-- hier komen de kaartjes  -->
+	
+</section>
+
+<style>
+	/* styling van mijn pagina */
+</style>
