0fefb51f675cad74bf0cb81c797a38b3adeac8b5
joelle78 <96306220+joelle78@users.noreply.github.com>
2023-10-09 19:24:02 +0200

    responsive
    tablet + desktop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import {onMount} from 'svelte';
     import Header from "$lib/components/header.svelte"
-	import NavDark from '../lib/components/NavDark.svelte';
+    import NavDark from '../lib/components/NavDark.svelte';
 
     export let data;
 
@@ -45,20 +45,37 @@
 
 <Header/>
 
-<NavDark />
+<!--<NavDark/>-->
 <!-- CAROUSEL -->
 <section class="carousel">
 
     <div class="slides">
 
-        <div class="slides-item slide-1" id="slide-1"><img src={data.headers[0].imagesCarousel.url}
-                                                           alt="foto van een stekje"/></div>
-        <div class="slides-item slide-2" id="slide-2"><img src={data.headers[1].imagesCarousel.url}
-                                                           alt="foto van een stekje"/></div>
-        <div class="slides-item slide-3" id="slide-3"><img src={data.headers[2].imagesCarousel.url}
-                                                           alt="foto van een stekje"/></div>
+        <div class="slides-item slide-1" id="slide-1">
+            <div class="image-container">
+                <img src={data.headers[0].imagesCarousel.url} alt="foto van een stekje"/>
+                <div class="image-text-1">Welkom bij PlantSwap</div>
+            </div>
+        </div>
 
+
+        <div class="slides-item slide-2" id="slide-2">
+
+            <div class="image-container-2">
+                <img src={data.headers[1].imagesCarousel.url} alt="foto van een stekje"/>
+                <div class="image-text-2">Welkom bij PlantSwap</div>
+            </div>
+        </div>
+
+
+        <div class="slides-item slide-3" id="slide-3">
+            <div class="image-container-3">
+                <img src={data.headers[2].imagesCarousel.url} alt="foto van een stekje"/></div>
+            <div class="image-text-3">Welkom bij PlantSwap</div>
+        </div>
     </div>
+
+
 </section>
 
 <!-- SECTION 1 HOW IT WORKS -->
@@ -99,11 +116,12 @@
     </div>
 
     <div class="button-stekjes">
-        <a href="/stekje"> <button>
-            <p>Bekijk alle stekjes!</p>
-        </button></a>
+        <a href="/stekje">
+            <button class="button-stek">
+                <p>Bekijk alle stekjes!</p>
+            </button>
+        </a>
     </div>
-
 
 
 </section>
@@ -124,15 +142,28 @@
         planten moeten zorgen en kopen nieuwe als ze dood gaan. In het kader van duurzaamheid, gezondheid en meer
         sociale connectie, is PlantSwap Amsterdam een plek van sociale ontmoeting, waar mensen samen leren over
         planten.</p>
+
+    <div class="image-about">
+        <img src="src/assets/netwerk.png">
+    </div>
 </section>
-
-
 
 
 <!-------------CSS------------->
 <style>
 
     /* ------------ CAROUSEL ------------*/
+
+    .image-text-1, .image-text-2, .image-text-3 {
+        position: absolute;
+        border-radius: 5px;
+        font-size: 3.0rem;
+        top: 30%;
+        left: 2.4%;
+        background-color: rgba(255, 255, 255, 0.5);
+        width: 22rem;
+    }
+
     .carousel {
         margin: 0;
         overflow: hidden;
@@ -143,6 +174,7 @@
     .slides {
         display: flex;
         overflow-x: scroll;
+        overflow-y: hidden;
         scroll-snap-type: x mandatory;
         scroll-behavior: smooth;
         scrollbar-width: none;
@@ -169,11 +201,49 @@
         transition: transform .5s;
         width: 120%;
         scroll-snap-align: start;
+    }
 
+    /* MEDIA QUERY TABLET*/
+    @media (min-width: 768px) and (max-width: 1023px) {
+        .image-text-1, .image-text-2, .image-text-3 {
+            font-size: 4rem;
+            left: 20%;
+            width: 30rem;
+        }
+
+        .slides img {
+            width: 50rem;
+        }
+
+        .slides-item {
+            width: 100%;
+        }
+    }
+
+    /* MEDIA QUERY DESKTOP*/
+    @media (min-width: 1024px) {
+        .image-text-1, .image-text-2, .image-text-3 {
+            font-size: 6rem;
+            left: 30%;
+            width: 40rem;
+        }
+
+        .slides img {
+            width: 150rem;
+
+        }
+
+        .slides-item {
+            width: 110%;
+            height: 40rem;
+        }
     }
 
 
     /* ------------ SECTION HOW IT WORKS ------------*/
+    h2 {
+        font-size: 30px;
+    }
 
     .flex-titel-how-it-works {
         display: flex;
@@ -186,18 +256,68 @@
 
     .flex-titel-how-it-works svg {
         width: 1rem;
+        margin-top: 0.4rem;
     }
 
     .how-it-works p {
         font-size: 16px;
+        line-height: 1.5rem;
         margin-left: 3.5rem;
         max-width: 18rem;
+    }
+
+    /* MEDIA QUERY TABLET */
+    @media (min-width: 768px) and (max-width: 1023px) {
+        h2 {
+            font-size: 50px;
+        }
+
+        .flex-titel-how-it-works {
+            margin-top: 5rem;
+
+        }
+
+        .flex-titel-how-it-works svg {
+            width: 3rem;
+            margin-top: 1.2rem;
+        }
+
+        .how-it-works p {
+            font-size: 25px;
+            line-height: 2rem;
+            margin-left: 5.5rem;
+            max-width: 38rem;
+        }
+    }
+
+    /* MEDIA QUERY DESKTOP*/
+    @media (min-width: 1024px) {
+        h2 {
+            font-size: 60px;
+        }
+
+        .flex-titel-how-it-works {
+            margin-top: 8rem;
+            margin-left: 10rem;
+        }
+
+        .flex-titel-how-it-works svg {
+            width: 5rem;
+            margin-top: 1.5rem;
+        }
+
+        .how-it-works p {
+            font-size: 30px;
+            line-height: 2.8rem;
+            margin-left: 16rem;
+            max-width: 50rem;
+        }
     }
 
     /* ------------ SECTION AVAILABLE STEKJES ------------*/
 
     h3 {
-        font-size: 23px;
+        font-size: 30px;
     }
 
     .flex-titel-available-stekjes {
@@ -211,10 +331,12 @@
 
     .flex-titel-available-stekjes svg {
         width: 1rem;
+        margin-top: 0.4rem;
     }
 
     .available-stekjes p {
         font-size: 16px;
+        line-height: 1.5rem;
         margin-left: 3.5rem;
         max-width: 18rem;
     }
@@ -236,7 +358,11 @@
         border-radius: 5px;
     }
 
-    .button-stekjes button {
+    .button-stekjes {
+        text-align: left;
+    }
+
+    .button-stek {
         background-color: var(--accent-color-sec);
         height: 2rem;
         border-radius: 5px;
@@ -244,13 +370,82 @@
         margin-top: 2rem;
         margin-left: 3.5rem;
         color: white;
-
+        text-align: left;
     }
 
-    
+    .button-stek p {
+        text-align: left;
+    }
+
+    /* MEDIA QUERY TABLET */
+    @media (min-width: 768px) and (max-width: 1023px) {
+        h3 {
+            font-size: 50px;
+        }
+
+        .flex-titel-available-stekjes svg {
+            width: 3rem;
+            margin-top: 1.2rem;
+        }
+
+        .available-stekjes p {
+            font-size: 25px;
+            line-height: 2rem;
+            margin-left: 5.5rem;
+            max-width: 38rem;
+        }
+
+
+        .flex-wrapper-recent-stekjes {
+            margin-left: 5.5rem;
+        }
+
+        .flex-wrapper-recent-stekjes img {
+            width: 25rem;
+        }
+    }
+
+    /* MEDIA QUERY DESKTOP*/
+    @media (min-width: 1024px) {
+        h3 {
+            font-size: 60px;
+        }
+
+        .flex-titel-available-stekjes svg {
+            width: 5rem;
+            margin-top: 1.5rem;
+        }
+
+        .flex-titel-available-stekjes {
+            margin-top: 20rem;
+            margin-left: 10rem;
+        }
+
+        .available-stekjes p {
+            font-size: 30px;
+            line-height: 2.8rem;
+            margin-left: 16rem;
+            max-width: 50rem;
+        }
+
+        .flex-wrapper-recent-stekjes {
+            justify-content: center;
+            align-items: center;
+            gap: 5rem;
+            margin-left: 5.5rem;
+            overflow-x: hidden;
+
+        }
+
+        .flex-wrapper-recent-stekjes img {
+            width: 25rem;
+        }
+    }
+
+
     /* ------------ SECTION ABOUT BUURTCAMPUS OOST ------------*/
     h4 {
-        font-size: 23px;
+        font-size: 30px;
     }
 
     .flex-title-about {
@@ -264,12 +459,78 @@
 
     .flex-title-about svg {
         width: 1rem;
+        margin-top: 0.4rem;
     }
 
     .about-homepage p {
         font-size: 16px;
+        line-height: 1.5rem;
         margin-left: 3.5rem;
         max-width: 18rem;
+    }
+
+    .image-about {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    .image-about img {
+        width: 20rem;
+        padding-top: 2rem;
+    }
+
+    /* MEDIA QUERY TABLET */
+    @media (min-width: 768px) and (max-width: 1023px) {
+        h4 {
+            font-size: 50px;
+        }
+
+        .flex-title-about svg {
+            width: 3rem;
+            margin-top: 1.2rem;
+        }
+
+        .about-homepage p {
+            font-size: 25px;
+            line-height: 2rem;
+            margin-left: 5.5rem;
+            max-width: 38rem;
+        }
+
+        .image-about img {
+            width: 40rem;
+            padding-top: 4rem;
+        }
+    }
+
+    /* MEDIA QUERY DESKTOP */
+    @media (min-width: 1024px) {
+        h4 {
+            font-size: 60px;
+        }
+
+        .flex-title-about svg {
+            width: 5rem;
+            margin-top: 1.5rem;
+        }
+
+        .flex-title-about {
+            margin-top: 20rem;
+            margin-left: 10rem;
+        }
+
+        .about-homepage p {
+            font-size: 30px;
+            line-height: 2.8rem;
+            margin-left: 16rem;
+            max-width: 50rem;
+        }
+
+        .image-about img {
+            display: none;
+        }
     }
 
 
