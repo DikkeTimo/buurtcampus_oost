ecd80a961e1f14521aabeef2be84b8bdf3b934d9
TimOosterveer <62908209+TimOosterveer@users.noreply.github.com>
2023-10-10 20:56:10 +0200

    Update +page.svelte

--- src/routes/workshops/+page.svelte
+++ src/routes/workshops/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Header from '$lib/components/header.svelte';
+	import Header from '$lib/components/Header.svelte';
 	import NavDark from '$lib/components/NavDark.svelte';
 	import Footer from '../../lib/components/footer.svelte';
 	export let data;
