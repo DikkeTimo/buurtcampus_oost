453f63a2636d821bdf13648a83df78797af179de
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-10-12 09:24:38 +0200

    Update BigImage.svelte

--- src/lib/components/BigImage.svelte
+++ src/lib/components/BigImage.svelte
@@ -2,6 +2,7 @@
   import { gsap } from "gsap";
   import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
   import { onMount } from "svelte";
+  import img from "$lib/assets/stekjes.jpg"
 
   gsap.registerPlugin(ScrollTrigger);
 
